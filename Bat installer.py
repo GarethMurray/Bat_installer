b166c339cf4369175bf6ece3fbc39957722cbc66
GarethMurray <64191834+GarethMurray@users.noreply.github.com>
2021-06-25 09:06:02 +0200

    Initial commit

---
+++ Bat installer.py
@@ -0,0 +1,25 @@
+import  os
+import subprocess
+"""
+Creates a bat file to run all apps within the "Apps" subdirectory
+"""
+
+# Initiation
+f=[]
+batfile = "@Echo on\n"
+
+# Create a list of all the files
+for (root,   folder, files) in os.walk("Apps"):
+	f.extend(files)
+
+# Create the text for the batfile based on the list above
+for appnames in f:
+	batfile = batfile + ("start /wait Apps\\"  + appnames + "\n") 
+
+# Write to bat file
+newfile = open("Installation command.bat","w") 
+newfile.write(batfile)
+newfile.close()
+
+# Open folder containing the bat file 
+subprocess.Popen(r'explorer "'+os.getcwd()+'"')
---
+++ README.md
@@ -0,0 +1,2 @@
+# Bat_installer
+ Creates a bat file to run all apps within the "Apps" subdirectory
